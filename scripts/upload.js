3599b6ac34c4f43559bf9997afec103f930a2169
Enrique <enriquec@mit.edu>
2022-05-11 00:00:20 -0400

    Made .env error message more clear

--- scripts/upload.js
+++ scripts/upload.js
@@ -60,7 +60,7 @@ console.log('> Connecting to database...');
 const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local',
+    'Please define the MONGODB_URI environment variable inside .env',
   );
 }
 
