52ac3310030900c8d4e77c91c99381e9868fa523
Enrique <enriquec@mit.edu>
2022-05-19 22:53:04 -0400

    Created explore page skeleton

--- pages/explore.js
+++ pages/explore.js
@@ -1,14 +1,31 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import { getAllWorkshops } from '../lib/apiUtils';
 
-const Explore = () => {
+const Map = dynamic(() => import('../components/Map'), {
+  loading: () => 'Loading...',
+  ssr: false,
+});
+const Explore = ({ workshops }) => {
   return (
     <>
       <Head>
         <title>Explore | Project Title</title>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
       </Head>
-      <div>explore</div>
+      <div style={{ position: 'absolute', top: 0, left: 0 }}>explore</div>
+      <Map workshops={workshops} />
     </>
   );
 };
+
+/* Retrieves workshops data from mongodb database */
+export async function getServerSideProps() {
+  const workshops = await getAllWorkshops();
+  return { props: { workshops } };
+}
 
 export default Explore;
