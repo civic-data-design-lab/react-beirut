3477d34d31ec70e704d5bad83f675a9ff1edf63c
Enrique <enriquec@mit.edu>
2022-05-27 12:39:31 -0400

    Work page

---
+++ pages/work.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+
+const WorkPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Work | Intangible Heritage Atlas</title>
+      </Head>
+
+      <div className="container">
+        <div className="title-card">
+          <div className="text-container">
+            <h1>Work</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et velit interdum, ac aliquet odio mattis.
+            </p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default WorkPage;
