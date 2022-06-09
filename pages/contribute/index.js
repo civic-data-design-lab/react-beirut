5c6a48c7a4a09c628e32ca95332c375c85ace358
Elina Oikonomaki <elinaoik@mit.edu>
2022-06-09 01:06:57 -0400

    Added Google Analytics

--- pages/contribute/index.js
+++ pages/contribute/index.js
@@ -198,6 +198,15 @@ const Contribute = () => {
   return (
     <>
       <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WP8TMP9M0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8WP8TMP9M0');
+        </script>
         <title>Contribute | Intangible Heritage Atlas</title>
       </Head>
       {modal && (
--- pages/trace.js
+++ pages/trace.js
@@ -4,6 +4,15 @@ const Trace = () => {
   return (
     <>
       <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WP8TMP9M0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8WP8TMP9M0');
+        </script>
         <title>Trace | Intangible Heritage Atlas</title>
       </Head>
       <div className="container">
