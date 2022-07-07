8fc273a829b4bc6123bb7390412cd380aa2f8737
Kelly Fang <kellyfang@dhcp-10-29-127-160.dyn.mit.edu>
2022-07-06 20:51:19 -0400

    fixed ref, prays this works

--- components/Slider.js
+++ components/Slider.js
@@ -14,12 +14,11 @@ export default class Slider extends React.Component {
     this.prevBtnRef = createRef();
 
     this.state = {
-      clicked:false,
-      prevDisable: true,
-      nextDisable:
-        this.sliderContainerRef && this.sliderContainerRef.offsetWidth >= this.sliderContainerRef.scrollWidth
-          ? true
-          : false,
+      prevDisable: false,
+      nextDisable: false,
+       //this.sliderContainerRef && this.sliderContainerRef.offsetWidth >= this.sliderContainerRef.scrollWidth
+          //? true
+          //: false,
     };
   }
 
@@ -27,35 +26,39 @@ export default class Slider extends React.Component {
     const container = this.sliderContainerRef.current;
     const wrapper = this.wrapperContainerRef.current;
     const itemWidth = wrapper.clientWidth;
-    container.scrollBy({ left: -itemWidth, top: 0, behavior: 'smooth' });
-    this.checkChildren(itemWidth, itemWidth/2);
+    console.log("itemWidth is ", itemWidth)
+    wrapper.scrollLeft -= itemWidth/2 //({ left: -itemWidth/2, top: 0, behavior: 'smooth' });
+      // this.checkChildren(itemWidth, itemWidth/2);
   };
 
   scrollRight = () => {
     const container = this.sliderContainerRef.current;
     const wrapper = this.wrapperContainerRef.current;
-    const itemWidth = container.clientWidth;
-    container.scrollBy({ left: itemWidth, top: 0, behavior: 'smooth' });
-    this.checkChildren(itemWidth, itemWidth/2);
+    const itemWidth = wrapper.clientWidth;
+    console.log("itemWidth is ", itemWidth)
+    wrapper.scrollLeft += itemWidth/2;//{ left: itemWidth/2, top: 0, behavior: 'smooth' });
+
+    // this.checkChildren(itemWidth, itemWidth/2);
   };
 
   componentDidMount() {
-    this.checkChildren(this.sliderContainerRef.current.offsetWidth, this.sliderContainerRef.current.scrollWidth);
+    // this.checkChildren(this.sliderContainerRef.current.offsetWidth, this.sliderContainerRef.current.scrollWidth);
   }
 
   checkChildren = (offsetWidthValue, scrollWidthValue) => {
     this.setState({
-      prevDisable: this.sliderContainerRef.current.scrollLeft <= 0 ? true : false,
+      prevDisable: this.sliderContainerRef.current.scrollLeft <= 0 ? false: true,
       nextDisable:
         this.sliderContainerRef.current.scrollLeft + offsetWidthValue >= scrollWidthValue
-          ? true
-          : false,
+          ? false
+          : true,
     });
   };
 
   render() {
 
     return (
+        <div>
       <div
         className="slider-container"
         id={"slider-container"}
@@ -69,7 +72,7 @@ export default class Slider extends React.Component {
         <button
           className={`btn prev ${this.state.prevDisable ? 'disable' : ''}`}
           disabled={this.state.prevDisable}
-          ref={this.prevBtnRef}>
+          ref={this.prevBtnRef}
           onClick={() => {
             this.scrollLeft()
           }}
@@ -90,7 +93,7 @@ export default class Slider extends React.Component {
         <div className={`blur-btns prev ${this.state.prevDisable ? 'disable' : ''}`}/>
         <div className={`blur-btns next ${this.state.nextDisable ? 'disable' : ''}`}/>
 
-
+      </div>
 
       </div>
     );
